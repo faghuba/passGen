fd2104d4288a37171a8d4eaaac82b1f19e48b51e
faghuba <techaghuba@gmail.con>
2021-07-07 12:45:10 +0300

    finishBuild

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+#!/usr/bin/env node
 const program = require('commander');
-const createPassword = require('./utils/createPassword')
+const chalk = require('chalk')
+const clipboardy = require('clipboardy')
 const log = console.log
+
+const createPassword = require('./utils/createPassword')
+const savedPassword = require('./utils/savedPassword')
 
 program.version('{ }').description('Simple password generator')
 
-
 program
-    .option('-L, --length <number>', 'length of password', '8')
+    .option('-L, --length <number>', 'length of password', '12')
     .option('-S, --save', 'Save password to passwords.txt')
     .option('-RN, --no-numbers', 'Remove numbers')
     .option('-RS, --no-symbols', 'Remove Symbols')
@@ -17,4 +21,17 @@ const { length, save, numbers, symbols } = program.opts()
 // Get Generated Psswords
 const generatedPassword = createPassword(length, numbers, symbols);
 
-log(generatedPassword)
+
+// Save to file 
+if (save) {
+    savedPassword(generatedPassword)
+}
+
+//Copy to clipboard 
+// clipboardy.writeSync(generatedPassword)
+
+
+// Output generated Paswword
+log(chalk.blue('Password Generated: ' + chalk.blueBright(generatedPassword)));
+// log(chalk.yellow('Paswword Copied to Clipboard'))
+
